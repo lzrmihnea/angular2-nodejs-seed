54e5ec6697ba298a70dd38d144e16c3019ecbb46
Mihnea <lzr_mihnea@yahoo.com>
2016-10-28 08:19:05 +0300

    Adding front-end random messages

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { Message } from "./message.model";
 
 @Component({
     moduleId: module.id,
     selector: 'my-app',
     templateUrl: 'app.template.html'
 })
+
 export class AppComponent {
-    
+    messages: Message[] = [
+        new Message('Hello')
+    ];
+
+    onAddMessage() {
+        const rnd = Math.ceil(Math.random() * 100);
+        const message = new Message(rnd + ' is good!');
+        this.messages.push(message);
+    }
 }
